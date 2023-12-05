f4151c86c729003939e35fb7e5bffe0d62cf018b
mthflashice <matheuslino@kerix.com.br>
2023-12-05 11:22:14 -0300

    Created DataBase sqlite

--- src/server.js
+++ src/server.js
@@ -1,11 +1,14 @@
 require("express-async-errors");
 const AppError = require ('./utils/AppError')
+const database = require ('./database/sqlite')
 const express = require("express");
 const routes = require('./routes');
 
 const app = express();
 
 app.use(routes);
+
+database ();
 
 app.use((error, request, response, next) => {
   if (error instanceof AppError) {
