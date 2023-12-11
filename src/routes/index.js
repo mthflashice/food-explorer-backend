36b261e0f2279095efa4d10a7cac9ba9270657de
mthflashice <matheuslino@kerix.com.br>
2023-12-11 12:14:14 -0300

    add authentication routes Sessions

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,12 @@
 const {Router} = require('express')
 const userRouter = require ('./users.routes')
 const dishesRoutes = require ('./dishes.routes')
+const sessionsRoutes = require ('./session.routes')
 
 const routes = Router();
 routes.use ('/user', userRouter);
+routes.use ('/sessions', sessionsRoutes);
+routes.use ('/dishes', dishesRoutes);
+
 
 module.exports = routes;
