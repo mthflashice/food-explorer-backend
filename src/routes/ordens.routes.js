b5089e9683b2b3a6b4db5beba7eb7043a9ab7197
mthflashice <matheuslino@kerix.com.br>
2023-12-14 11:22:41 -0300

    add show and delete in Order

--- src/routes/ordens.routes.js
+++ src/routes/ordens.routes.js
@@ -1,6 +1,7 @@
 const {Router} = require('express');
 const OrdersController = require('../controllers/OrdersController')
 const ensureAuthenticated = require('../middlewares/ensureAuthenticated')
+const checkAdminPermission = require('../middlewares/checkAdminPermission')
 
 const ordersRoutes = Router();
 
@@ -9,5 +10,7 @@ const ordersController = new OrdersController();
 ordersRoutes.use(ensureAuthenticated);
 
 ordersRoutes.post('/',ordersController.create);
+ordersRoutes.get('/:id', ordersController.show);
+ordersRoutes.delete('/:id',checkAdminPermission, ordersController.delete);
 
 module.exports = ordersRoutes;
