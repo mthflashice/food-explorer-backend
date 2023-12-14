f5c501baba5484b239da08f2c1a9d6379b99aaac
mthflashice <matheuslino@kerix.com.br>
2023-12-14 11:11:45 -0300

    add register Order

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,11 +2,13 @@ const {Router} = require('express')
 const userRouter = require ('./users.routes')
 const dishesRoutes = require ('./dishes.routes')
 const sessionsRoutes = require ('./session.routes')
+const ordersRouter = require('./ordens.routes')
 
 const routes = Router();
 routes.use ('/user', userRouter);
 routes.use ('/sessions', sessionsRoutes);
 routes.use ('/dishes', dishesRoutes);
+routes.use ('ordens', ordersRouter);
 
 
 module.exports = routes;
