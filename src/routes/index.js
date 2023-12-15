1891017a7fb15a302088bab04cc79bf679cc12a7
mthflashice <matheuslino@kerix.com.br>
2023-12-15 10:30:14 -0300

    add Favorites controlar and routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,13 +2,16 @@ const {Router} = require('express')
 const userRouter = require ('./users.routes')
 const dishesRoutes = require ('./dishes.routes')
 const sessionsRoutes = require ('./session.routes')
-const ordersRouter = require('./ordens.routes')
+const ordersRouter = require('./orders.routes')
+const favoritesRouter = require('./favorites.routes')
+
 
 const routes = Router();
 routes.use ('/user', userRouter);
 routes.use ('/sessions', sessionsRoutes);
 routes.use ('/dishes', dishesRoutes);
 routes.use ('ordens', ordersRouter);
+routes.use ('/favorites', favoritesRouter);
 
 
 module.exports = routes;
