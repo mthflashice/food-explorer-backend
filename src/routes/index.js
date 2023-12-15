02cfc7f897f77da20f420540d72ea48bcc436812
mthflashice <matheuslino@kerix.com.br>
2023-12-15 11:53:14 -0300

    add register carts

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,6 +4,7 @@ const dishesRoutes = require ('./dishes.routes')
 const sessionsRoutes = require ('./session.routes')
 const ordersRouter = require('./orders.routes')
 const favoritesRouter = require('./favorites.routes')
+const cartsRouter = require('./carts.routes')
 
 
 const routes = Router();
@@ -12,6 +13,8 @@ routes.use ('/sessions', sessionsRoutes);
 routes.use ('/dishes', dishesRoutes);
 routes.use ('ordens', ordersRouter);
 routes.use ('/favorites', favoritesRouter);
+routes.use ('/carts', cartsRouter);
+
 
 
 module.exports = routes;
