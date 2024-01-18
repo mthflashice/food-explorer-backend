a274d5c500053cbb6085e9ebd1836de265e88ba0
mthflashice <matheuslino@kerix.com.br>
2024-01-18 14:13:11 -0300

    fix Carts | add one Desserts database

--- src/controllers/CartsController.js
+++ src/controllers/CartsController.js
@@ -1,35 +1,31 @@
-const { response } = require('express');
 const knex = require('../database/knex');
 const AppError = require('../utils/AppError');
 
-
 class CartsController {
-    async create(request, response){
-        const { cart_items } = request.body;
-        const user_id = request.user.id;
+  async create(request, response) {
+    const { cart_items } = request.body;
+    const user_id = request.user.id;
 
-        const [cart_id] = await knex('carts').insert({
-            created_by: user_id,
-
+    const [cart_id] = await knex('carts').insert({
+      created_by: user_id,
     });
 
     const itemsInsert = cart_items.map(async ({ dish_id, name, quantity }) => {
-  
-        return {
-          cart_id,
-          dish_id,
-          name,
-          quantity,
-        };
-      });
-  
-      await knex('cart_items').insert(await Promise.all(itemsInsert));
-  
-      return response.json({id: cart_id});
-    }
+      return {
+        cart_id,
+        dish_id,
+        name,
+        quantity,
+      };
+    });
 
-    async show(request, response) {
-      const { id } = request.params;
+    await knex('cart_items').insert(await Promise.all(itemsInsert));
+
+    return response.json({ id: cart_id });
+  }
+
+  async show(request, response) {
+    const { id } = request.params;
 
     const cart = await knex('carts').where({ id }).first();
     const cart_items = await knex('cart_items').where({ cart_id: id });
@@ -40,15 +36,7 @@ class CartsController {
     });
   }
 
-  async delete(request, response) {
-    const { id } = request.params;
-
-    await knex('cart_items').where({ cart_id: id }).delete();
-    await knex('carts').where({ id }).delete();
-
-    return response.json();
-   }
-   async update(request, response) { 
+  async update(request, response) { 
     const { id } = request.params;
     const { cart_items } = request.body;
 
@@ -61,47 +49,51 @@ class CartsController {
     const cartUpdate = {
       updated_at: knex.fn.now(),
     };
-
+  
     const existingItems = await knex('cart_items')
-    .where({ cart_id: id })
-    .select('dish_id');
-
-  const updatedItems = cart_items.map(({ dish_id, name, quantity }) => {
-    if (existingItems.some((item) => item.dish_id === dish_id)) {
-      return knex('cart_items')
-        .where({ cart_id: id, dish_id })
-        .update({ quantity });
-    } else {
-      return knex('cart_items').insert({
-        cart_id: id,
-        dish_id,
-        name,
-        quantity,
-      });
-    }
+      .where({ cart_id: id })
+      .select('dish_id');
+  
+    const updatedItems = cart_items.map(({ dish_id, name, quantity }) => {
+      if (existingItems.some((item) => item.dish_id === dish_id)) {
+        return knex('cart_items')
+          .where({ cart_id: id, dish_id })
+          .update({ quantity });
+      } else {
+        return knex('cart_items').insert({
+          cart_id: id,
+          dish_id,
+          name,
+          quantity,
+        });
+      }
     });
 
     await Promise.all(updatedItems);
     await knex('carts').where({ id }).update(cartUpdate);
 
-    await knex('cart_items').insert(await Promise.all(itemsInsert));
-    await knex('carts').where({ id }).update(cartUpdate);
-
     return response.json();
-  }
+  }    
 
-  async index( request, response){
+  async index(request, response) {
     const user_id = request.user.id;
 
     const carts = await knex('carts')
-    .select('id', 'created_at')
-    .where({created_by: user_id})
-    .orderBy('created_at', 'desc');
+      .select('id', 'created_at')
+      .where({ created_by: user_id })
+      .orderBy('created_at', 'desc');
 
-    return response.json(carts)
+    return response.json(carts);
   }
 
-}
-  
+  async delete(request, response) {
+    const { id } = request.params;
 
-  module.exports = CartsController;
+    await knex('cart_items').where({ cart_id: id }).delete();
+    await knex('carts').where({ id }).delete();
+
+    return response.json();
+  }
+}
+
+module.exports = CartsController;
